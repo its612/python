4897d4c89f3a71e889c060981027e87338142d4e
root <root@localhost>
2022-12-21 20:26:57 +0500

    auto publish 1671636417493

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print(5 + 3)
+jennifer_money = 12000 
+julia_money = 400 
+if jennifer_money > julia_money:
+    print('Jennifer, you are my best friend') 
+else:
+    print('Julia, you are my soul mate')
